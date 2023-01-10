14a6a472e40d377ab644f1137c660b71882fb667
dtaubaso <d.taubaso@gmail.com>
2023-01-10 19:17:36 -0300

    Agregadao people also search

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from utils import *
 from typing import Dict, Any
-import urllib, uvicorn, re, chromedriver_binary
+import urllib, uvicorn, re
 
+webhook_url = "https://hooks.slack.com/services/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"
 
 app = FastAPI()
 
@@ -16,11 +17,36 @@ def google_question(req: Dict[Any, Any] = None):
     query = urllib.parse.quote(str(kw))
     base_url = 'https://www.google.com/search?q='
     url = f'{base_url}{query}&hl={lang}&gl={country}&uule={uule}'
-    response = getGoogleQuestions(url, cant_clicks)
-    if response:
-        return {kw: [q for q in response]}
-    else:
-        return {kw: ["Nothing Found"]}
+    try:
+        response = getGoogleQuestions(url, cant_clicks)
+        if response:
+            return {kw: [q for q in response]}
+        else:
+            return {kw: ["Nothing Found"]}
+    except Exception as e:
+        print(e)
+        text = f'*Google Questions* Error: {e}'
+        send_slack(text, webhook_url)
+
+@app.post("/people_also_search")
+def people_also_search(req: Dict[Any, Any] = None):
+    kw = req['kw']
+    uule = req['uule']
+    lang = req['lang']
+    country = req['country']
+    query = urllib.parse.quote(str(kw))
+    base_url = 'https://www.google.com/search?q='
+    url = f'{base_url}{query}&hl={lang}&gl={country}&uule={uule}'
+    try:
+        response = getRelated(url)
+        if response:
+            return {kw: [q for q in response]}
+        else:
+            return {kw: ["Nothing Found"]}
+    except Exception as e:
+        print(e)
+        text = f'*Google People Also Search* Error: {e}'
+        send_slack(text, webhook_url)
 
 if __name__ == '__main__':
     uvicorn.run(app=app, host="127.0.0.1", port=8000)
--- utils.py
+++ utils.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-import time
+from selenium.webdriver.support import expected_conditions as EC
+import time, requests, json, re
 
+
+# Google Questions
 def getGoogleQuestions(url, clicks):
   options = webdriver.ChromeOptions()
   options.add_argument("--headless")
@@ -21,3 +24,41 @@ def getGoogleQuestions(url, clicks):
     except Exception as e:
       print(e)
       return
+
+# Tambien se buscó
+def getRelated(url):
+  options = webdriver.ChromeOptions()
+  options.add_argument("--headless")
+  options.add_argument("--disable-gpu")
+  options.add_argument("--disable-dev-shm-usage")
+  options.add_argument("--no-sandbox")
+  driver = webdriver.Chrome(options=options)
+  with driver:
+    driver.get(url)
+    x_path = "//*[text()='Otras personas también buscan' or text()='También se buscó']"
+    x_path_link = "//*[text()='Ver más' or text()='Otras personas también buscan' or text()='También se buscó']"
+    try:
+        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, x_path)))
+        link = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, x_path_link)))
+        link.click()
+        time.sleep(2)
+        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'ct5Ked.klitem-tr.PZPZlf')))
+        elements = driver.find_elements(By.CLASS_NAME,"ct5Ked.klitem-tr.PZPZlf")
+        element_list = [x.text for x in elements]
+        related_ent = [sub.replace('\n', ' ') for sub in element_list]
+        related_ent = [re.sub(' Tendencias| Tendencia| Desde.*', '', a) for a in related_ent]
+        related_ent = list(filter(None, related_ent))
+        return related_ent
+    except Exception as e:
+        print(f'Hubo un problema {e}')
+
+
+
+def send_slack(text, webhook_url):
+  slack_data = {'text': text}
+  response = requests.post(
+    webhook_url, 
+    data=json.dumps(slack_data), 
+    headers={'Content-Type': 'application/json'}
+    )
