d32bae77a8c7a562c11b15dff277eefd3c7b3960
dtaubaso <d.taubaso@gmail.com>
2025-01-06 13:08:49 -0300

    arregla people also search

--- utils.py
+++ utils.py
@@ -1,19 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time, requests, json, re
-import chromedriver_binary
+import time, requests, json, re, lxml.html, random
 
+devices = requests.get("https://raw.githubusercontent.com/dtaubaso/aux/refs/heads/main/selenium_chrome_devices.txt").text
+devices = list(filter(None, devices.split("\n")))
 
 # Google Questions
 def getGoogleQuestions(url, clicks):
+  service = Service()
   options = webdriver.ChromeOptions()
   options.add_argument("--headless")
   options.add_argument("--disable-gpu")
   options.add_argument("--disable-dev-shm-usage")
   options.add_argument("--no-sandbox")
-  driver = webdriver.Chrome(options=options)
+  driver = webdriver.Chrome(service=service, options=options)
   with driver:
     driver.get(url)
     try:
@@ -28,37 +31,32 @@ def getGoogleQuestions(url, clicks):
 
 # Tambien se buscó
 def getRelated(url):
+  service = Service()
   options = webdriver.ChromeOptions()
   options.add_argument("--headless")
   options.add_argument("--disable-gpu")
   options.add_argument("--disable-dev-shm-usage")
+  mobile_emulation = {"deviceName": random.choice(devices)}
+  options.add_experimental_option("mobileEmulation", mobile_emulation)
   options.add_argument("--no-sandbox")
-  driver = webdriver.Chrome(options=options)
+  driver = webdriver.Chrome(service=service, options=options)
   driver.implicitly_wait(3)
   with driver:
-    driver.get(url)
-    x_path = "//*[text()='Otras personas también buscan' or text()='También se buscó']"
-    x_path_link = "//*[text()='Otras personas también buscan' or text()='También se buscó' or text()='Ver más']"
     try:
-        # mbappe
-        if driver.find_elements(By.CLASS_NAME,"TzHB6b"):
-          link = WebDriverWait(driver, 5).until(
-            EC.element_to_be_clickable((By.LINK_TEXT, "Ver más")))
-          link.click()
-        elif driver.find_elements(By.CLASS_NAME,"liYKde"):
-          link = WebDriverWait(driver, 5).until(
-            EC.element_to_be_clickable((By.XPATH, x_path)))
-          link.click()
-        time.sleep(2)
-        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'ct5Ked')))
-        elements = driver.find_elements(By.CLASS_NAME,"ct5Ked")
-        element_list = [x.text for x in elements]
-        related_ent = [sub.replace('\n', ' ') for sub in element_list]
-        related_ent = [re.sub(' Tendencias| Tendencia| Desde.*', '', a) for a in related_ent]
-        related_ent = list(filter(None, related_ent))
-        return related_ent
+      driver.get(url)
+      body = driver.page_source
+      if body == None:
+        raise Exception("Sin body")
     except Exception as e:
         print(f'Hubo un problema {e}')
+        return None
+  tree = lxml.html.fromstring(body)
+  element_list = [a.text_content() for a in tree.find_class("IF221e EXH1Ce")]
+  related_ent = [sub.replace('\n', ' ') for sub in element_list]
+  related_ent = [re.sub(' Tendencias| Tendencia| Desde.*', '', a) for a in related_ent]
+  related_ent = list(filter(None, related_ent))
+  return related_ent
+    
 
 
 
