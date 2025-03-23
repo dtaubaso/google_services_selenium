4c09bc5c6dc950cb1a1fb0f38b328f0823c261b0
dtaubaso <d.taubaso@gmail.com>
2025-03-23 14:25:55 -0300

    agrega user agent a return body

--- utils.py
+++ utils.py
@@ -71,6 +71,7 @@ def return_body(url):
   options.add_argument("--no-sandbox")
   options.add_argument("--disable-blink-features=AutomationControlled")
   options.add_argument("--enable-javascript")
+  options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36")
   driver = webdriver.Chrome(service=service, options=options)
   with driver:
     driver.get(url)
